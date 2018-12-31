fdf9f67f966832f34af184be9e38ab10bac5bdaa
shankar <shankaranarayanan712@gmail.com>
2018-12-31 13:44:39 +0530

    first commit

---
+++ LinkedList/NodeClass.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    // Step 1: Here is the node class which has 2 properties: 
+    // One property is 'Next', which will have reference of next node and another is 'Value', which will have data in this node.
+    public class Node
+    {
+        public object Value { get; set; }
+        public Node Next { get; set; }
+    }
+    // Now implement linked list class as follows:  
+    //'head' will have head node. 'current' will have latest node, i.e., tail node and 'Count' will return total number of nodes in linked list.
+    public class LinkedList
+    {
+        public Node head;
+        public Node current;
+        public int Count;
+        public LinkedList()
+        {
+            head = new Node();
+            //Here current and head points to same memory , so change in any propertis of current  will change head
+            current = head;
+        }
+
+        public void AddAtLast(object data)
+        {
+            Node newNode = new Node();
+            newNode.Value = data;
+            // Here whenever there is a change the current , head will get automatically changed as it is a reference of it
+            current.Next = newNode; // Now Head.Next will have data value (ex: 12)
+            current = newNode; // Here we preserve the head' added value pointer to current
+            Count++;
+        }
+
+        public void AddAtStart(object data)
+        {
+            Node newNode = new Node() { Value = data };
+            newNode.Next = head.Next;//new node will have reference of head's next reference
+            head.Next = newNode;//and now head will refer to new node
+            Count++;
+        }
+
+        public void RemoveFromStart()
+        {
+            while (Count > 0)
+            {
+                head.Next = head.Next.Next;
+                Count--;
+            }
+            //else
+            //{
+            //    Console.WriteLine("No element exist in this linked list.");
+            //}
+        }
+
+        public void RemoveByIndex(int index)
+        {
+            int count = 0;
+            Node current = head;
+            Node previous = current;
+            while (count <= index)
+            {
+                if (count == index)
+                {
+                    previous.Next = current.Next;
+                    current = null;
+                }
+                else
+                {
+                    previous = current;
+                    current = current.Next;
+                }
+                count++;
+            }
+        }
+
+        /// <summary>
+        /// Traverse from head and print all nodes value
+        /// </summary>
+
+        public void PrintAllNodes()
+        {
+            //Traverse from head
+            Console.Write("Head ->");
+            Node curr = head;
+            while (curr.Next != null)
+            {
+                curr = curr.Next;
+                Console.Write(curr.Value);
+                Console.Write("->");
+            }
+            Console.Write("NULL");
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Node FirstNode = new Node { Value = 1 };
+            //Node MiddleNode = new Node { Value = 2 };
+            //Node LastNode = new Node { Value = 3 };
+
+            //FirstNode.Next = MiddleNode;
+            //MiddleNode.Next = LastNode;
+
+            //printList(FirstNode);
+
+            LinkedList lnklist = new LinkedList();
+            lnklist.PrintAllNodes();
+            Console.WriteLine();
+
+            lnklist.AddAtLast(12);
+            lnklist.AddAtLast("John");
+            lnklist.AddAtLast("Peter");
+            lnklist.AddAtLast(34);
+            lnklist.PrintAllNodes();
+            Console.WriteLine();
+
+            lnklist.AddAtStart(55);
+            lnklist.AddAtStart("John");
+            lnklist.AddAtStart("Peter");
+            lnklist.PrintAllNodes();
+            Console.WriteLine();
+            lnklist.RemoveByIndex(2);
+            lnklist.RemoveFromStart();
+            lnklist.PrintAllNodes();
+         
+
+            Console.ReadKey();
+
+        }
+        private static void printList(Node node)
+        {
+            while(node != null)
+            {
+                Console.WriteLine(node.Value);
+                node = node.Next;
+            }
+        }
+    }
+}   
